2faea5eb99cdee333754c5885077806e0c266327
Robbie McCorkell <robbiemccorkell@gmail.com>
2017-09-18 11:26:36 +0100

    Add define plugin for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: ['babel-polyfill', './src/index.js'],
@@ -26,6 +27,11 @@ module.exports = {
     extensions: ['.re', '.ml', '.js'],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
     }),
